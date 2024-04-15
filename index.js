94b880c273de5845d6fd9167042579696a0bc15e
zfowed <zfowed@qq.com>
2024-04-16 01:32:29 +0800

    feat: 依赖

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const fs = require('fs')
 const path = require('path')
 const glob = require('glob')
 const lodash = require('lodash')
+const rename = require('gulp-rename')
 // const { Font } = require('fonteditor-core')
 // const { Ttf, Woff, Eot } = require('modern-font')
-const opentype = require('opentype.js')
+// const opentype = require('opentype.js')
+const Fontmin = require('fontmin')
 
 function posixPathJoin (...srcs) {
   if (path.sep !== path.posix.sep) {
@@ -26,10 +28,12 @@ function getPath (...srcs) {
 
 const fileList = glob.sync(getPath('fonts/*/*.@(ttf|eot|otf|woff|woff2)'), {
   ignore: [],
+}).map((file) => {
+  return getPath(file)
 })
 
 const fontGroup = lodash.groupBy(fileList, file => {
-  return  path.basename(file, path.extname(file))
+  return path.basename(file, path.extname(file))
 })
 
 
@@ -44,12 +48,32 @@ async function main () {
     if (!ttfFontFile) {
       if (eotFontFile) {
         // eot to ttf
+        let ttfFontFile = eotFontFile.replace(/\.eot$/, '.ttf')
+        const fontmin = new Fontmin()
+          .src(eotFontFile)
+          .use(Fontmin.eot2ttf())
+          .use(rename(path.basename(ttfFontFile)))
+          .dest(path.dirname(ttfFontFile))
+        await new Promise((resolve, reject) => {
+          return fontmin.run(function (err, files) {
+            if (err) return reject(err)
+            return resolve(files)
+          })
+        })
       } else if (otfFontFile) {
         // otf to ttf
-        console.log(otfFontFile);
-        const font = await opentype.load(getPath(otfFontFile))
-        const ttfBuffer = font.toArrayBuffer();
-        fs.writeFileSync(getPath(otfFontFile).replace(/\.otf$/, '.ttf'), Buffer.from(ttfBuffer))
+        let ttfFontFile = otfFontFile.replace(/\.otf$/, '.ttf')
+        const fontmin = new Fontmin()
+          .src(otfFontFile)
+          .use(Fontmin.otf2ttf())
+          .use(rename(path.basename(ttfFontFile)))
+          .dest(path.dirname(ttfFontFile))
+        await new Promise((resolve, reject) => {
+          return fontmin.run(function (err, files) {
+            if (err) return reject(err)
+            return resolve(files)
+          })
+        })
       } else if (woffFontFile) {
         // woff to ttf
       } else if (woff2FontFile) {
